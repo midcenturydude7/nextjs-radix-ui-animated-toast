840314194309b680a29401d34524180f8ca72559
midCenturyDude7 <mgriffes@gmail.com>
2024-01-25 22:16:17 -0800

    Working with react-toast in radix-ui

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { XMarkIcon } from "@heroicons/react/20/solid";
 export default function Page() {
   return (
     <div className="flex p-2">
-      <button className="text w-28 rounded border-t border-white/20 bg-sky-500 py-2">
+      <button className="w-28 rounded border-t border-white/20 bg-sky-500 py-2 text-sm font-semibold text-white transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 active:bg-sky-600">
         Notify
       </button>
 
@@ -18,7 +18,7 @@ export default function Page() {
           className="flex items-center justify-between rounded border-gray-700 bg-gray-800 px-6 py-4 text-sm font-medium"
         >
           <Toast.Description>Changes saved!</Toast.Description>
-          <Toast.Close>
+          <Toast.Close className="text-gray-600 hover:text-gray-200">
             <XMarkIcon className="size-5" />
           </Toast.Close>
         </Toast.Root>
